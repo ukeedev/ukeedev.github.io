bfa4809024fdfb0d6e3870332f1243ca9027211a
John Monte <ph.johnmonte@yahoo.com>
2023-04-17 14:39:58 +0800

    202304171439
    reverted to non-sheets version

--- assets/js/converter.js
+++ assets/js/converter.js
@@ -33,7 +33,7 @@ var vm2 = new Vue({
     }
   });
 
-//document.getElementById("current_buy_1").innerHTML = buy_rate;
-//document.getElementById("current_buy_2").innerHTML = buy_rate;
-//document.getElementById("current_sell_1").innerHTML = sell_rate;
-//document.getElementById("current_sell_2").innerHTML = sell_rate;
+document.getElementById("current_buy_1").innerHTML = buy_rate;
+document.getElementById("current_buy_2").innerHTML = buy_rate;
+document.getElementById("current_sell_1").innerHTML = sell_rate;
+document.getElementById("current_sell_2").innerHTML = sell_rate;
